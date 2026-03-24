00d692d0723f5184114126c200d594e184e1e9ef
Seth Grimes <seth.grimes1999@gmail.com>
2026-03-24 14:54:25 -0400

    Added tables for merchants

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --noconsole --onefile --clean --add-data "sounds;sounds" --add-data "recipes.csv;." --add-data "game_icon.ico;." --icon=game_icon.ico --name "Text Adventure" qt_main.py
+pyinstaller --noconfirm --noconsole --onefile --clean --add-data "sounds;sounds" --add-data "recipes.csv;." --add-data "game_icon.ico;." --icon=game_icon.ico --name "Text Adventure" main.py
